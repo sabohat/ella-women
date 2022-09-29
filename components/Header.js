0d5367751320af12427a2cbf8c7dbc3e08aa627b
sabokhatkalandarova <sabohat2802@gmail.com>
2022-09-29 07:08:31 +0500

    add header component

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
-import './Header'
+import styles from './Header.module.scss'
+import { LogoIcon } from './svg'
 
 export default function Header() {
     return (
-        <div className='header'>Header</div>
+        <div className={styles.header}>
+            <div>
+                <LogoIcon />
+            </div>
+            <ul className={styles.list}>
+                <li className={styles.link}><Link href="/articles">Maqolalar</Link> </li>
+                <li className={styles.link}><Link href="/product">Mahsulotlar</Link></li>
+                <li className={styles.link}><Link href="/calculators">Kalkulyatorlar</Link></li>
+                <li className={`${styles.link} ${styles.active}`}><Link href="/about-us">Biz Haqimizda</Link></li>
+            </ul>
+
+        </div>
     )
 }
 
