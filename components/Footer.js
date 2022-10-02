4cd21d7160d0383b0878810185b9009c3623b8c8
Munirabonu <muniratillaxojayeva@gmail.com>
2022-10-02 14:09:40 +0500

    footer_row1

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react'
-import { InstagramLogo, TelegramLogo, FacebookLogo } from './svg'
-
+import { InstagramLogo, TelegramLogo, FacebookLogo, LogoIcon_White, AppStoreDownloadIcon, GooglePlayDownloadIcon } from './svg'
+import styles from './Footer.module.scss'
+import Link from 'next/link'
 export default function Footer() {
+
+    const headerLinks = [
+        {
+            link: "/articles",
+            label: "Articles",
+            id: 0
+        },
+        {
+            link: "/product",
+            label: "Mahsulotlar",
+            id: 1
+        },
+        {
+            link: "/calculators",
+            label: "Kalkulyatorlar",
+            id: 2
+        },
+        {
+            link: "/about-us",
+            label: "Biz haqimizda",
+            id: 3
+        }
+    ]
+
     return (
-        <div>
-            I am footer, style me!
+        <div className={styles.footer}>
+            <div className={styles.row1}>
+                <div className={styles.links}>
+                    <LogoIcon_White />
+                    <ul className={styles.list}>
+                        {headerLinks.map((item) => (
+                            <li key={item.id} className={styles.link}><Link activeClassName={styles.active} href={item.link}>{item.label}</Link></li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.download_link}>
+                    <AppStoreDownloadIcon />
+                    <GooglePlayDownloadIcon />
+                </div>
+            </div>
         </div>
     )
 }
