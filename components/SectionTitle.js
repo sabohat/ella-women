3d115bfcae3cbd4eeda2c5034693565f0af8794b
Umida <u.abdugafurova@uicgroup.org>
2022-10-07 05:24:08 +0500

    fix: format section title

--- components/SectionTitle.js
+++ components/SectionTitle.js
@@ -1,8 +1,6 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./SectionTitle.module.scss"
+import styles from "./SectionTitle.module.scss";
 export default function SectionTitle(props) {
-    return (
-           <h2 className={styles.sectionTitle}>{props.sectionTitle}</h2>
-    )
+  return <h2 className={styles.sectionTitle}>{props.sectionTitle}</h2>;
 }
--- components/ServiceSection.js
+++ components/ServiceSection.js
@@ -1,44 +1,39 @@
-import React from 'react'
-import SectionTitle from './SectionTitle'
-import ServiceCard from './ServiceCard'
-import styles from "./ServiceCard.module.scss"
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import ServiceCard from "./ServiceCard";
+import styles from "./ServiceCard.module.scss";
 
-import { PregnancyCalendarIcon, PregnancyStagesIcon, VaginaIcon } from './svg'
-
+import { PregnancyCalendarIcon, PregnancyStagesIcon, VaginaIcon } from "./svg";
 
 export default function ServiceSection() {
-
-    const cardList = [
-        {
-            title: "Отслеживание цикла",
-            subtitle: "Поймите свой цикл и почувствуйте себя лучшей",
-            // svg: (height, width) => <VaginaIcon height={height} width={width} />
-            svg: <VaginaIcon />
-        },
-        {
-            title: "Увеличьте свои шансы забеременеть",
-            subtitle: "Особые условия для покупателей апартаментов, автомобиль в аренду за сто рублей на срок до года",
-            svg: <PregnancyCalendarIcon />
-        },
-        {
-            title: "Будьте здоровы во время беременности",
-            subtitle: "Особые условия для покупателей апартаментов, автомобиль в аренду за сто рублей на срок до года",
-            svg: <PregnancyStagesIcon />
-        }
-    ]
-    return (
-        <>
-            <div className='wrapper'>
-            <SectionTitle sectionTitle="Какие сервисы предоставляет Ladycare?"/>
-            <div className={styles.cardsWrapper}>
-                {
-                    cardList.map((card, i) =>
-                        <ServiceCard  key={i} card={card} />
-                    )
-                }
-            </div>
-            </div>
-        </>
-
-    )
+  const cardList = [
+    {
+      title: "Отслеживание цикла",
+      subtitle: "Поймите свой цикл и почувствуйте себя лучшей",
+      // svg: (height, width) => <VaginaIcon height={height} width={width} />
+      svg: <VaginaIcon />,
+    },
+    {
+      title: "Увеличьте свои шансы забеременеть",
+      subtitle:
+        "Особые условия для покупателей апартаментов, автомобиль в аренду за сто рублей на срок до года",
+      svg: <PregnancyCalendarIcon />,
+    },
+    {
+      title: "Будьте здоровы во время беременности",
+      subtitle:
+        "Особые условия для покупателей апартаментов, автомобиль в аренду за сто рублей на срок до года",
+      svg: <PregnancyStagesIcon />,
+    },
+  ];
+  return (
+    <div className="wrapper">
+      <SectionTitle sectionTitle="Какие сервисы предоставляет Ladycare?" />
+      <div className={styles.cardsWrapper}>
+        {cardList.map((card, i) => (
+          <ServiceCard key={i} card={card} />
+        ))}
+      </div>
+    </div>
+  );
 }
