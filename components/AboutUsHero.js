26677944ae8428130e3f7adda2b4619432732142
sabokhatkalandarova <sabohat2802@gmail.com>
2022-10-07 07:03:14 +0500

    fix: hero image alignment

--- components/AboutUsHero.js
+++ components/AboutUsHero.js
@@ -2,9 +2,8 @@ import React from 'react'
 import { AppStoreDownloadIcon, GooglePlayDownloadIcon } from './svg'
 import styles from './AboutUsHero.module.scss'
 import Image from 'next/image'
-import image from "../assets/img/image.png"
-import image1 from "../assets/img/image1.png"
-import image2 from "../assets/img/image2.png"
+import image from "../assets/img/hero.png"
+
 
 
 export default function AboutUsHero() {
@@ -19,23 +18,13 @@ export default function AboutUsHero() {
                         <a href="https://www.google.com/"><GooglePlayDownloadIcon /></a>
                     </div>
                     <div className={styles.heroRight}>
-                        <div className={styles.heroWrapper}>
-                            <div className={styles.heroPhone}>
-                                <Image src={image} width={300} height={470} />
-                            </div>
-                            <div className={styles.heroImage}>
-                                <Image src={image1} width={215} height={270} />
-                            </div>
-                            <div className={styles.heroimg}>
-                                <Image src={image2} width={217} height={287} />
-                            </div>
-                        </div>
-
+                        <Image src={image} width={550} height={480} />
                     </div>
-                </div>
 
+                </div>
             </div>
 
         </div>
+
     )
 }
