30e9f35f64f2e934388ba619d7973afe46d53eb7
sabokhatkalandarova <sabohat2802@gmail.com>
2023-01-05 14:46:14 +0500

    add results

--- components/PregnancyCalc.js
+++ components/PregnancyCalc.js
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+require('dayjs/locale/ru')
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import MuiButton from './common/Button';
@@ -16,6 +17,7 @@ export default function PregnancyCalc() {
 
     const [isLoading, setIsLoading] = useState(false)
     const [isCalculated, setCalculated] = useState(false)
+    const [data, setData] = useState({})
 
     const calculate = () => {
         setIsLoading(true)
@@ -32,6 +34,8 @@ export default function PregnancyCalc() {
 
         // divide into trimesters
 
+        const firstTrimesterStart = dayjs(givenDate)
+
         // 1st trimester, 1week - 12week
         const firstTrimesterEnd = dayjs(givenDate).add(12, 'week').subtract(1, 'day')
 
@@ -40,7 +44,20 @@ export default function PregnancyCalc() {
         const secondTrimesterEnd = dayjs(givenDate).add(26, 'week').subtract(1, 'day')
         // 3rd TrimesterStart
         const thirdTrimesterStart = dayjs(givenDate).add(26, 'week')
+        const thirdTrimesterEnd = dayjs(birthDate)
 
+
+
+        setData({
+            birthDate: birthDate,
+            pregWeeks: pregWeeks,
+            firstTrimesterStart: firstTrimesterStart,
+            firstTrimesterEnd: firstTrimesterEnd,
+            secondTrimesterStart: secondTrimesterStart,
+            secondTrimesterEnd: secondTrimesterEnd,
+            thirdTrimesterStart: thirdTrimesterStart,
+            thirdTrimesterEnd: thirdTrimesterEnd
+        })
 
         setCalculated(true)
         setIsLoading(false)
@@ -92,35 +109,35 @@ export default function PregnancyCalc() {
 
 
                                 <p className={styles.boldText}>Вы встретите своего ребенка в</p>
-                                <h2 className={styles.resultText}>17 Июня, 2023</h2>
-                                <span className={styles.bottomText}>Суббота</span>
+                                <h2 className={styles.resultText}>{dayjs(data?.birthDate).locale('ru').format('D MMMM, YYYY')}</h2>
+                                <span className={styles.bottomText}>{dayjs(data?.birthDate).locale('ru').format('dddd')}</span>
                             </div>
                             <div className={styles.trimester}>
                                 <div className={styles.trimesterCard}>
                                     <div className={styles.trimesterText}>
                                         <h3>{t("1st trimester")}</h3>
-                                        <span className={styles.grayText}>1 aprel, 2022 - 23 yanvar, 2022</span>
+                                        <span className={styles.grayText}>{dayjs(data?.firstTrimesterStart).locale('ru').format('D MMM, YYYY')} {' - '} {dayjs(data?.firstTrimesterEnd).format('D MMM, YYYY')}</span>
                                     </div>
                                     <div className={styles.line}></div>
                                 </div>
                                 <div className={styles.trimesterCard}>
                                     <div className={styles.trimesterText}>
                                         <h3>{t("1st trimester")}</h3>
-                                        <span className={styles.grayText}>1 aprel, 2022 - 23 yanvar, 2022</span>
+                                        <span className={styles.grayText}>{dayjs(data?.firstTrimesterStart).locale('ru').format('D MMM, YYYY')} {' - '} {dayjs(data?.firstTrimesterEnd).format('D MMM, YYYY')}</span>
                                     </div>
                                     <div className={styles.line}></div>
                                 </div>
                                 <div className={styles.trimesterCard}>
                                     <div className={styles.trimesterText}>
                                         <h3>{t("1st trimester")}</h3>
-                                        <span className={styles.grayText}>1 aprel, 2022 - 23 yanvar, 2022</span>
+                                        <span className={styles.grayText}>{dayjs(data?.firstTrimesterStart).locale('ru').format('D MMM, YYYY')} {' - '} {dayjs(data?.firstTrimesterEnd).format('D MMM, YYYY')}</span>
                                     </div>
                                     <div className={styles.line}></div>
                                 </div>
                             </div>
                         </div>
                         <div className={` ${styles.resultContainer} ${styles.weekContainer}`}>
-                            <span>Вы сейчас на 6 неделе 😊</span>
+                            <span>Вы сейчас на {data?.pregWeeks} неделе 😊</span>
                         </div>
                     </div>
                 </div>
